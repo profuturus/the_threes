e7533904e7665a9c6a2d7db9b6a1b9dc2aa477c3
prometheus <jason@lvl6.xyz>
2019-01-23 12:49:29 -0700

    end of 'logout' section. pg 443

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,6 @@ app_name = 'users'
 urlpatterns = [
     # Login page
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    path('logout/', views.logout_view, name='logout',)
 ]
