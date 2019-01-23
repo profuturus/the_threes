24a5c309cd13bdd7d831bc1f9c7e6c9a7d420dc9
prometheus <jason@lvl6.xyz>
2019-01-23 11:40:26 -0700

    Up to 'logging out', pg 442

--- the_threes/settings.py
+++ the_threes/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # my apps
     'the_threes_tracker',
+    'users',
 ]
 
 MIDDLEWARE = [
