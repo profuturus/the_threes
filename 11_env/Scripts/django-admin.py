eb42ba125e5e4f2326f0f2fc734f88df41968e77
jmd10 <jmd_10@yahoo.com>
2022-04-27 12:04:24 -0400

    deleted old virtualenv and created new. Prepared to branch for changes

--- 11_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\prometheus\dropbox\lvl6\python\the_threes\11_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
