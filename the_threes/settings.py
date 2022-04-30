2c660ebb9449aaf4dfe34ca7742c271fd9f72ed0
jmd10 <jmd_10@yahoo.com>
2022-04-30 09:58:42 -0400

    return 'secret_key' to settings to test Heroku

--- the_threes/settings.py
+++ the_threes/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+# SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = "($-yqt8^c69f+g*$6x3=(+1db711h!=np16%^7#1x90y8@%vs5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
