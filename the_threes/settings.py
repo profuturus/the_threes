c8c59f0e07da92d680a926f08db4bb3bd7969103
jmd10 <jmd_10@yahoo.com>
2022-05-02 20:20:52 -0400

    change 'secret_key' away from os.getenv for heroku problem solving

--- the_threes/settings.py
+++ the_threes/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "($-yqt8^c69f+g*$6x3=(+1db711h!=np16%^7#1x90y8@%vs5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
 ALLOWED_HOSTS = ['localhost']
 
